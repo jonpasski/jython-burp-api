71a2b117f290c6f9dd5a46f8b6ab5811477dc98e
Marcin Wielgoszewski <marcin.wielgoszewski@gmail.com>
2012-05-25 15:24:03 -0400

    expand tabs into 4 spaces

--- java/src/com/gdssecurity/http/HttpRequestResponse.java
+++ java/src/com/gdssecurity/http/HttpRequestResponse.java
@@ -7,178 +7,178 @@ import burp.IHttpRequestResponse;
 
 public class HttpRequestResponse
 {
-	public static final boolean PROCESSED = true;
-	
-	private IHttpRequestResponse messageInfo;
-	
-	private String requestBody;
-	private TreeMap<String, String> requestHeaders;
-	private String requestMethod;
-	private String requestProtocolVersion;
-	private String requestUri;
-	
-	private String responseBody;
-	private TreeMap<String, String> responseHeaders;
-	private String responseProtocolVersion;
-	private String responseReason;
-	private int responseStatus;
-	
-	public HttpRequestResponse(IHttpRequestResponse messageInfo,
-			String requestMethod, String requestUri,
-			String requestProtocolVersion,
-			TreeMap<String, String> requestHeaders, String requestBody,
-			int responseStatus, String responseReason,
-			String responseProtocolVersion,
-			TreeMap<String, String> responseHeaders, String responseBody)
-	{
-		super();
-		
-		this.messageInfo = messageInfo;
-		this.requestMethod = requestMethod;
-		this.requestUri = requestUri;
-		this.requestProtocolVersion = requestProtocolVersion;
-		this.requestHeaders = requestHeaders;
-		this.requestBody = requestBody;
-		this.responseStatus = responseStatus;
-		this.responseReason = responseReason;
-		this.responseProtocolVersion = responseProtocolVersion;
-		this.responseHeaders = responseHeaders;
-		this.responseBody = responseBody;
-	}
-	
-	public String getComment() throws Exception
-	{
-		return messageInfo.getComment();
-	}
-	
-	public String getHost()
-	{
-		return messageInfo.getHost();
-	}
-	
-	public IHttpRequestResponse getMessageInfo()
-	{
-		return messageInfo;
-	}
-	
-	public int getPort()
-	{
-		return messageInfo.getPort();
-	}
-	
-	public String getProtocol()
-	{
-		return messageInfo.getProtocol();
-	}
-	
-	public String getRequestBody()
-	{
-		return requestBody;
-	}
-	
-	public TreeMap<String, String> getRequestHeaders()
-	{
-		return requestHeaders;
-	}
-	
-	public String getRequestMethod()
-	{
-		return requestMethod;
-	}
-	
-	public String getRequestProtocolVersion()
-	{
-		return requestProtocolVersion;
-	}
-	
-	public String getRequestUri()
-	{
-		return requestUri;
-	}
-	
-	public String getResponseBody()
-	{
-		return responseBody;
-	}
-	
-	public TreeMap<String, String> getResponseHeaders()
-	{
-		return responseHeaders;
-	}
-	
-	public String getResponseProtocolVersion()
-	{
-		return responseProtocolVersion;
-	}
-	
-	public String getResponseReason()
-	{
-		return responseReason;
-	}
-	
-	public int getResponseStatus()
-	{
-		return responseStatus;
-	}
-	
-	public URL getUrl() throws Exception
-	{
-		return messageInfo.getUrl();
-	}
-	
-	public void setComment(String comment) throws Exception
-	{
-		messageInfo.setComment(comment);
-	}
-	
-	public void setRequestBody(String requestBody)
-	{
-		this.requestBody = requestBody;
-	}
-	
-	public void setRequestHeaders(TreeMap<String, String> requestHeaders)
-	{
-		this.requestHeaders = requestHeaders;
-	}
-	
-	public void setRequestMethod(String requestMethod)
-	{
-		this.requestMethod = requestMethod;
-	}
-	
-	public void setRequestProtocolVersion(String requestProtocolVersion)
-	{
-		this.requestProtocolVersion = requestProtocolVersion;
-	}
-	
-	public void setRequestUri(String requestUri)
-	{
-		this.requestUri = requestUri;
-	}
-	
-	public void setResponseBody(String responseBody)
-	{
-		this.responseBody = responseBody;
-	}
-	
-	public void setResponseHeaders(TreeMap<String, String> responseHeaders)
-	{
-		this.responseHeaders = responseHeaders;
-	}
-	
-	public void setResponseProtocolVersion(String responseProtocolVersion)
-	{
-		this.responseProtocolVersion = responseProtocolVersion;
-	}
-	
-	public void setResponseReason(String responseReason)
-	{
-		this.responseReason = responseReason;
-	}
-	
-	public void setResponseStatus(int responseStatus)
-	{
-		this.responseStatus = responseStatus;
-	}
-	
+    public static final boolean PROCESSED = true;
+    
+    private IHttpRequestResponse messageInfo;
+    
+    private String requestBody;
+    private TreeMap<String, String> requestHeaders;
+    private String requestMethod;
+    private String requestProtocolVersion;
+    private String requestUri;
+    
+    private String responseBody;
+    private TreeMap<String, String> responseHeaders;
+    private String responseProtocolVersion;
+    private String responseReason;
+    private int responseStatus;
+    
+    public HttpRequestResponse(IHttpRequestResponse messageInfo,
+            String requestMethod, String requestUri,
+            String requestProtocolVersion,
+            TreeMap<String, String> requestHeaders, String requestBody,
+            int responseStatus, String responseReason,
+            String responseProtocolVersion,
+            TreeMap<String, String> responseHeaders, String responseBody)
+    {
+        super();
+        
+        this.messageInfo = messageInfo;
+        this.requestMethod = requestMethod;
+        this.requestUri = requestUri;
+        this.requestProtocolVersion = requestProtocolVersion;
+        this.requestHeaders = requestHeaders;
+        this.requestBody = requestBody;
+        this.responseStatus = responseStatus;
+        this.responseReason = responseReason;
+        this.responseProtocolVersion = responseProtocolVersion;
+        this.responseHeaders = responseHeaders;
+        this.responseBody = responseBody;
+    }
+    
+    public String getComment() throws Exception
+    {
+        return messageInfo.getComment();
+    }
+    
+    public String getHost()
+    {
+        return messageInfo.getHost();
+    }
+    
+    public IHttpRequestResponse getMessageInfo()
+    {
+        return messageInfo;
+    }
+    
+    public int getPort()
+    {
+        return messageInfo.getPort();
+    }
+    
+    public String getProtocol()
+    {
+        return messageInfo.getProtocol();
+    }
+    
+    public String getRequestBody()
+    {
+        return requestBody;
+    }
+    
+    public TreeMap<String, String> getRequestHeaders()
+    {
+        return requestHeaders;
+    }
+    
+    public String getRequestMethod()
+    {
+        return requestMethod;
+    }
+    
+    public String getRequestProtocolVersion()
+    {
+        return requestProtocolVersion;
+    }
+    
+    public String getRequestUri()
+    {
+        return requestUri;
+    }
+    
+    public String getResponseBody()
+    {
+        return responseBody;
+    }
+    
+    public TreeMap<String, String> getResponseHeaders()
+    {
+        return responseHeaders;
+    }
+    
+    public String getResponseProtocolVersion()
+    {
+        return responseProtocolVersion;
+    }
+    
+    public String getResponseReason()
+    {
+        return responseReason;
+    }
+    
+    public int getResponseStatus()
+    {
+        return responseStatus;
+    }
+    
+    public URL getUrl() throws Exception
+    {
+        return messageInfo.getUrl();
+    }
+    
+    public void setComment(String comment) throws Exception
+    {
+        messageInfo.setComment(comment);
+    }
+    
+    public void setRequestBody(String requestBody)
+    {
+        this.requestBody = requestBody;
+    }
+    
+    public void setRequestHeaders(TreeMap<String, String> requestHeaders)
+    {
+        this.requestHeaders = requestHeaders;
+    }
+    
+    public void setRequestMethod(String requestMethod)
+    {
+        this.requestMethod = requestMethod;
+    }
+    
+    public void setRequestProtocolVersion(String requestProtocolVersion)
+    {
+        this.requestProtocolVersion = requestProtocolVersion;
+    }
+    
+    public void setRequestUri(String requestUri)
+    {
+        this.requestUri = requestUri;
+    }
+    
+    public void setResponseBody(String responseBody)
+    {
+        this.responseBody = responseBody;
+    }
+    
+    public void setResponseHeaders(TreeMap<String, String> responseHeaders)
+    {
+        this.responseHeaders = responseHeaders;
+    }
+    
+    public void setResponseProtocolVersion(String responseProtocolVersion)
+    {
+        this.responseProtocolVersion = responseProtocolVersion;
+    }
+    
+    public void setResponseReason(String responseReason)
+    {
+        this.responseReason = responseReason;
+    }
+    
+    public void setResponseStatus(int responseStatus)
+    {
+        this.responseStatus = responseStatus;
+    }
+    
 }
--- java/src/com/gdssecurity/http/HttpRequestResponseParser.java
+++ java/src/com/gdssecurity/http/HttpRequestResponseParser.java
@@ -25,122 +25,122 @@ import com.gdssecurity.http.mock.MockSessionInputBuffer;
 
 public class HttpRequestResponseParser
 {
-	
-	private static EntityDeserializer serializer;
-	
-	public static HttpRequestResponseParser instance = null;
-	
-	public synchronized HttpRequestResponseParser getInstance()
-	{
-		if (instance == null)
-			instance = new HttpRequestResponseParser();
-		
-		return instance;
-	}
-	
-	public HttpRequestResponseParser()
-	{
-		serializer = createEntityDeserializer();
-	}
-	
-	protected EntityDeserializer createEntityDeserializer()
-	{
-		return new EntityDeserializer(new LaxContentLengthStrategy());
-	}
-	
-	public HttpRequestResponse parse(IHttpRequestResponse message)
-	throws Exception
-	{
-		
-		SessionInputBuffer requestBuffer = new MockSessionInputBuffer(message
-				.getRequest(), message.getRequest().length);
-		
-		DefaultHttpRequestParser httpRequestParser = new DefaultHttpRequestParser(
-				requestBuffer, BasicLineParser.DEFAULT,
-				new DefaultHttpRequestFactory(), new BasicHttpParams());
-		
-		HttpRequest httpRequest = (HttpRequest) httpRequestParser.parse();
-		
-		RequestLine requestLine = httpRequest.getRequestLine();
-		
-		String requestMethod = requestLine.getMethod();
-		
-		String requestUri = requestLine.getUri();
-		
-		String requestProtocolVersion = requestLine.getProtocolVersion()
-		.toString();
-		
-		TreeMap<String, String> requestHeaders = getMessageHeaders(httpRequest
-				.getAllHeaders());
-		
-		HttpEntity requestEntity = serializer.deserialize(requestBuffer,
-				httpRequest);
-		String requestData = EntityUtils.toString(requestEntity);
-		
-		// /////////////////////////////////////////////////////////////////
-		//
-		// Parse http response
-		//
-		// /////////////////////////////////////////////////////////////////
-		
-		SessionInputBuffer responseBuffer = new MockSessionInputBuffer(message
-				.getResponse(), message.getResponse().length);
-		
-		DefaultHttpResponseParser httpResponseParser = new DefaultHttpResponseParser(
-				responseBuffer, BasicLineParser.DEFAULT,
-				new DefaultHttpResponseFactory(), new BasicHttpParams());
-		
-		BasicHttpResponse httpResponse = (BasicHttpResponse) httpResponseParser
-		.parse();
-		
-		StatusLine responseStatusLine = httpResponse.getStatusLine();
-		
-		int responseStatus = responseStatusLine.getStatusCode();
-		
-		String responseReasonPhrase = responseStatusLine.getReasonPhrase();
-		
-		String responseProtocolVersion = responseStatusLine
-		.getProtocolVersion().toString();
-		
-		TreeMap<String, String> responseHeaders = getMessageHeaders(httpResponse
-				.getAllHeaders());
-		
-		HttpEntity responseEntity = serializer.deserialize(responseBuffer,
-				httpResponse);
-		String responseBody = EntityUtils.toString(responseEntity);
-		
-		return new HttpRequestResponse(message, requestMethod, requestUri,
-				requestProtocolVersion, requestHeaders, requestData,
-				responseStatus, responseReasonPhrase, responseProtocolVersion,
-				responseHeaders, responseBody);
-		
-	}
-	
-	private TreeMap<String, String> getMessageHeaders(Header[] messageHeaders)
-	{
-		TreeMap<String, String> headers = new TreeMap<String, String>(
-				String.CASE_INSENSITIVE_ORDER);
-		
-		for (Header header : messageHeaders)
-		{
-			
-			String value = headers.get(header.getName());
-			
-			if (value != null && header.getValue() != value)
-			{
-				value = value + ", " + header.getValue();
-				headers.put(header.getName(), value);
-			}
-			
-			else
-			{
-				headers.put(header.getName(), header.getValue());
-			}
-			
-		}
-		
-		return headers;
-		
-	}
-	
+    
+    private static EntityDeserializer serializer;
+    
+    public static HttpRequestResponseParser instance = null;
+    
+    public synchronized HttpRequestResponseParser getInstance()
+    {
+        if (instance == null)
+            instance = new HttpRequestResponseParser();
+        
+        return instance;
+    }
+    
+    public HttpRequestResponseParser()
+    {
+        serializer = createEntityDeserializer();
+    }
+    
+    protected EntityDeserializer createEntityDeserializer()
+    {
+        return new EntityDeserializer(new LaxContentLengthStrategy());
+    }
+    
+    public HttpRequestResponse parse(IHttpRequestResponse message)
+    throws Exception
+    {
+        
+        SessionInputBuffer requestBuffer = new MockSessionInputBuffer(message
+                .getRequest(), message.getRequest().length);
+        
+        DefaultHttpRequestParser httpRequestParser = new DefaultHttpRequestParser(
+                requestBuffer, BasicLineParser.DEFAULT,
+                new DefaultHttpRequestFactory(), new BasicHttpParams());
+        
+        HttpRequest httpRequest = (HttpRequest) httpRequestParser.parse();
+        
+        RequestLine requestLine = httpRequest.getRequestLine();
+        
+        String requestMethod = requestLine.getMethod();
+        
+        String requestUri = requestLine.getUri();
+        
+        String requestProtocolVersion = requestLine.getProtocolVersion()
+        .toString();
+        
+        TreeMap<String, String> requestHeaders = getMessageHeaders(httpRequest
+                .getAllHeaders());
+        
+        HttpEntity requestEntity = serializer.deserialize(requestBuffer,
+                httpRequest);
+        String requestData = EntityUtils.toString(requestEntity);
+        
+        // /////////////////////////////////////////////////////////////////
+        //
+        // Parse http response
+        //
+        // /////////////////////////////////////////////////////////////////
+        
+        SessionInputBuffer responseBuffer = new MockSessionInputBuffer(message
+                .getResponse(), message.getResponse().length);
+        
+        DefaultHttpResponseParser httpResponseParser = new DefaultHttpResponseParser(
+                responseBuffer, BasicLineParser.DEFAULT,
+                new DefaultHttpResponseFactory(), new BasicHttpParams());
+        
+        BasicHttpResponse httpResponse = (BasicHttpResponse) httpResponseParser
+        .parse();
+        
+        StatusLine responseStatusLine = httpResponse.getStatusLine();
+        
+        int responseStatus = responseStatusLine.getStatusCode();
+        
+        String responseReasonPhrase = responseStatusLine.getReasonPhrase();
+        
+        String responseProtocolVersion = responseStatusLine
+        .getProtocolVersion().toString();
+        
+        TreeMap<String, String> responseHeaders = getMessageHeaders(httpResponse
+                .getAllHeaders());
+        
+        HttpEntity responseEntity = serializer.deserialize(responseBuffer,
+                httpResponse);
+        String responseBody = EntityUtils.toString(responseEntity);
+        
+        return new HttpRequestResponse(message, requestMethod, requestUri,
+                requestProtocolVersion, requestHeaders, requestData,
+                responseStatus, responseReasonPhrase, responseProtocolVersion,
+                responseHeaders, responseBody);
+        
+    }
+    
+    private TreeMap<String, String> getMessageHeaders(Header[] messageHeaders)
+    {
+        TreeMap<String, String> headers = new TreeMap<String, String>(
+                String.CASE_INSENSITIVE_ORDER);
+        
+        for (Header header : messageHeaders)
+        {
+            
+            String value = headers.get(header.getName());
+            
+            if (value != null && header.getValue() != value)
+            {
+                value = value + ", " + header.getValue();
+                headers.put(header.getName(), value);
+            }
+            
+            else
+            {
+                headers.put(header.getName(), header.getValue());
+            }
+            
+        }
+        
+        return headers;
+        
+    }
+    
 }
